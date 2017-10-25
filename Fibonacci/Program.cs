2f8d11b7a167b9426d4b1dbaa4e64a7f305620f3
ras101-83 <33103176+ras101-83@users.noreply.github.com>
2017-10-25 21:41:16 +0100

    Add basic solution

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        private const int DEFAULT_COUNT = 25;
+
+        static void Main(string[] args)
+        {
+            var count = DEFAULT_COUNT;
+            ParseArguments();
+
+            var fibonacciSequence = Fibonacci().Take(count);
+
+            Console.WriteLine(String.Join(Environment.NewLine, fibonacciSequence));
+
+            ExitApplication(0);
+
+            IEnumerable<long> Fibonacci()
+            {
+                yield return 0;
+
+                long previous = 0;
+                long current = 1;
+
+                while (true)
+                {
+                    yield return current;
+
+                    unchecked
+                    {
+                        var tmp = current;
+                        current = current + previous;
+                        previous = tmp;
+                    }
+
+                    if (current < 0) break;
+                }
+            }
+
+            void ExitApplication(int exitCode)
+            {
+                if (Debugger.IsAttached)
+                {
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey(true);
+                }
+
+                Environment.Exit(exitCode);
+            }
+
+            void ParseArguments()
+            {
+                if (args.Length > 1)
+                {
+                    ShowHelp();
+                    ExitApplication(1);
+                }
+
+                if (args.Length == 1)
+                {
+                    if (!Int32.TryParse(args[0], out count))
+                    {
+                        ShowHelp();
+                        ExitApplication(1);
+                    }
+                }
+            }
+
+            void ShowHelp()
+            {
+                Console.WriteLine("bla");
+            }
+        }
+    }
+}
